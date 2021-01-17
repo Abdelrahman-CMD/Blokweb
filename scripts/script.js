0cc7c9be7d2fca8622e9e5da0f2cb5a81249cbe1
Abdelrahman-CMD <58260362+Abdelrahman-CMD@users.noreply.github.com>
2021-01-17 23:13:03 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,17 @@
 // JavaScript Document
+function showMenu() {
+  document.querySelector("nav#menu").classList.add('show');
+}
+
+function hideMenu() {
+  document.querySelector("nav#menu").classList.remove('show');
+}
+
+document.querySelector("#menubutton").addEventListener("click", showMenu);
+document.querySelector("nav#menu").addEventListener("click", hideMenu);
+
+function hartVullen() {
+  document.querySelector("img#hart").classList.add('vullen');
+}
+
+document.querySelector("img#hart").toggle("click", hartVullen);
