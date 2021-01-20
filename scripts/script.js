54ac65106140378b90e8e7b7019a8ef4d6beee1e
Abdelrahman-CMD <58260362+Abdelrahman-CMD@users.noreply.github.com>
2021-01-20 15:39:23 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,35 +1,34 @@
-
-
-/*de nav met de id menu in de html opzoeken en de styling show laten zien*/
+/*de nav met de id menu in de html opzoeken en de styling css class show word toegevoegd aan de nav#menu  */
 
 function showMenu() {
   document.querySelector("nav#menu").classList.add('show');
 }
 
 
-/*de nav met de id menu in de html opzoeken en de styling show weghalen*/
+/*de nav met de id menu in de html opzoeken en de styling css class show word weggehaald van de nav#menu */
 
 function hideMenu() {
   document.querySelector("nav#menu").classList.remove('show');
 }
 
-/*als er gedrukt word op de button laat dan de menu zien*/
+/*als er gedrukt wordt op de icoon met id menubutton roep ik de functie showMenu aan die het menu uitklapt. */
 
 document.querySelector("#menubutton").addEventListener("click", showMenu);
 
-/*als er gedrukt word op de button laat dan de menu zien*/
+/*als er gedrukt wordt op de icoon met id menubutton roep ik de functie showMenu aan die het menu inklapt. */
 
 document.querySelector("nav#menu").addEventListener("click", hideMenu);
 
 /*wishlistknop zoekt de button op met de id wishlist*/
 
-let wishListKnop = document.querySelector("button#wishlist")
+let wishListKnop = document.querySelector("button#wishlist");
 
 /*een terugkoppeling in de console log voor als er gedrukt word*/
 
 console.log(wishListKnop)
 
-/*als er gedrukt word op de button laat de hart gevuld raken*/
+/*als er gedrukt wordt op de wishlist knop wordt de hart gevuld doormiddel van de functie 
+data filled staat voor /*custom data atribuut om bij te houden of het hartje gevuld is of niet, dmv een boolean */
 
 wishListKnop.addEventListener("click", wishlistKlik)
 
@@ -46,7 +45,7 @@ function wishlistKlik() {
   console.log(hartImg.src)
 }
 
-/*dropdownknop de button op met de id dropwdown-btn*/
+/*dropdownknop de button haalt de button op met de id dropwdown-btn*/
 
 let dropdownKnop = document.querySelector("button#dropdown-btn")
 dropdownKnop.addEventListener("click", toggleDropdown);
@@ -56,17 +55,15 @@ dropdownKnop.addEventListener("click", toggleDropdown);
 
 console.log(dropdownKnop)
 
-/*als er op de knop word gedrukt toon de bijbehorende tekst, dus de ul met id dropdown-text
-telken als er op gedrukt word verschijnt het of verbergt het.
-*/
+/*als er gedrukt wordt op de meer info knop wordt de de tekst in de ul uitgeklapt met de functie toggleDropdown*/
 
 
 function toggleDropdown() {
   console.log("Toggling dropdown..")
   let dropdownText = document.querySelector("ul#dropdown-text")
   if (dropdownText.classList.contains('show')) {
-      dropdownText.classList.remove('show');
+    dropdownText.classList.remove('show');
   } else {
-      dropdownText.classList.add('show');
+    dropdownText.classList.add('show');
   }
 }
